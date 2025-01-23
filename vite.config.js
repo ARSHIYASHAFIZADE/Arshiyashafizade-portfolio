8535f58d363e3de219d60e15d458d8956201fc3b
ARSHIYASHAFIZADE <145537704+ARSHIYASHAFIZADE@users.noreply.github.com>
2025-01-24 00:13:06 +0800

    Updated environment variables and fixed email sending functionality

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/Arshiyashafizade-portfolio/', // This is crucial for GitHub Pages
   plugins: [react()],
   build: {
-    outDir: "dist", // Specifies the output directory for build files
+    outDir: 'dist',
   },
 });
+  
