c899e9ed3c126e4f4075d1ade0d97ecb52e0bc6b
ARSHIYASHAFIZADE <145537704+ARSHIYASHAFIZADE@users.noreply.github.com>
2025-01-24 03:50:24 +0800

    Fix Vite server configuration and add React plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
+  plugins: [react()],
   server: {
-    host: '0.0.0.0', // Bind to 0.0.0.0 for Render
-    port: parseInt(process.env.PORT) || 3000, // Use Render's PORT or fallback to 3000
-    allowedHosts: ['arshiyashafizade-portfolio-2.onrender.com'], // Allow your Render host
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT) || 3000,
   },
 });
